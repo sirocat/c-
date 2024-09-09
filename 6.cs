abe819315605d62dacf1eb536316f196919de5e1
SIRO <jjh060510@gmail.com>
2024-09-10 08:12:51 +0900

    Create 6.cs

---
+++ 6.cs
@@ -0,0 +1,15 @@
+using System;
+
+public class Solution {
+    public int solution(int[] num_list) {
+        int sum = 0;
+        int mul = 1;
+        
+        for(int i = 0; i < num_list.Length; i++) {
+            sum += num_list[i];
+            mul *= num_list[i];
+        }
+        
+        return mul < (sum * sum) ? 1 : 0;
+    }
+}
